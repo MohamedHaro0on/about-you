7d20380cedc264ab0e8d2f1dd60143afc7f3d098
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2021-04-16 04:32:37 +0200

    Updates

---
+++ precache-manifest.b79a5bbd278596454c0113a94416c3e4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa32c65441e0ad1242e84243ad1e597b",
+    "url": "/about-you/index.html"
+  },
+  {
+    "revision": "0dc92284135babe5b466",
+    "url": "/about-you/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "386745c81d7a155ebfb1",
+    "url": "/about-you/static/css/main.45884c14.chunk.css"
+  },
+  {
+    "revision": "0dc92284135babe5b466",
+    "url": "/about-you/static/js/2.aca1b366.chunk.js"
+  },
+  {
+    "revision": "026ee29f3633c0a8e28a22a7a4b7393c",
+    "url": "/about-you/static/js/2.aca1b366.chunk.js.LICENSE"
+  },
+  {
+    "revision": "386745c81d7a155ebfb1",
+    "url": "/about-you/static/js/main.0ec6a703.chunk.js"
+  },
+  {
+    "revision": "38abbb100c28ef8713c5",
+    "url": "/about-you/static/js/runtime-main.24eddff3.js"
+  },
+  {
+    "revision": "65b4d47fb6303c7b1416a675d0720f8d",
+    "url": "/about-you/static/media/01_Header-1.65b4d47f.png"
+  },
+  {
+    "revision": "9243ec4006533538c86293e4f4ee3330",
+    "url": "/about-you/static/media/01_Header-K1.9243ec40.jpg"
+  },
+  {
+    "revision": "2f1e0dadb9cbdd0de5acc28255a49b3d",
+    "url": "/about-you/static/media/02_AboutYou-1-688x583.2f1e0dad.png"
+  },
+  {
+    "revision": "39bbfaac6329452cec49aa9a24555fd2",
+    "url": "/about-you/static/media/02_AboutYou-688x583.39bbfaac.png"
+  },
+  {
+    "revision": "7e4e62797648e77816399bbc8501823c",
+    "url": "/about-you/static/media/03_Stats.7e4e6279.png"
+  },
+  {
+    "revision": "34a74612e2df0c393ee677c023dd3c65",
+    "url": "/about-you/static/media/04_Vision.34a74612.png"
+  },
+  {
+    "revision": "1e6fdcf1d6f2ad039f9c135dcbb2b06b",
+    "url": "/about-you/static/media/05_B2B.1e6fdcf1.png"
+  },
+  {
+    "revision": "d8a8c93e4831da284641aed3c618cc3d",
+    "url": "/about-you/static/media/06_Markets.d8a8c93e.png"
+  },
+  {
+    "revision": "e8980a75f7c21cd718348085054a2ab3",
+    "url": "/about-you/static/media/ABOUT-YOU_Adventskalender_20195-380x245.e8980a75.jpg"
+  },
+  {
+    "revision": "ca5e93a59c65b55ae93dfe82899fad65",
+    "url": "/about-you/static/media/ABOUT-YOU_PARI-2.0_lowres_13-380x245.ca5e93a5.jpg"
+  },
+  {
+    "revision": "e40e3173f2a6c346b82362fc2da83315",
+    "url": "/about-you/static/media/B02V6991.jpg_cmyk-380x245.e40e3173.jpg"
+  },
+  {
+    "revision": "356c644dcffdaad63421427a1e9d6443",
+    "url": "/about-you/static/media/download.356c644d.svg"
+  }
+]);
